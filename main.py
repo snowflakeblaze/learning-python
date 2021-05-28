b39a796a0d1e3dfc315cf5ef8e318021a2086448
Pritesh Soma <>
2021-05-28 17:46:11 +0000

    BMI Calculator

---
+++ main.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in m: "))
+BMI = round(weight/(height*height),2)
+print("At age " + str(age) + " your BMI is " + str(BMI))
